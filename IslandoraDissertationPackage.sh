44fe67c54c7dc22c801fb765dcc41ba0ed42f021
Jason Fowler <jfowler@Jasons-MacBook-Pro-3.local>
2017-01-10 18:46:20 -0500

    Fixed comment end placed incorrectly and added two new files.

---
+++ IslandoraDissertationPackage.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# Usage: package the files for Islandora
+# Steps to use:
+
+#Go to correct directory
+cd ~/DigitalRepos
+
+# Unzip zip files
+unzip "*.zip"
+mv *.zip zips
+
+# Convert ProQuest XML files to MODS
+for file in *.xml; do 
+	newfile=$(echo "$file" | sed "s/_DATA//g")
+	xsltproc proquest-to-mods.xsl  $file > "$newfile"
+done
+
+#Remove ProQuest XML files
+rm -f *_DATA.xml
+
+# Package Zip files
+filename=$(date +%Y%m%d%H%M%S)
+mkdir "$filename"
+mv *.xml "$filename"
+mv *.pdf "$filename"
+zip "$filename.zip" $filename/*
+
+
+	
